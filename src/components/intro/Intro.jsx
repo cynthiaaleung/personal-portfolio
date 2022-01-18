ba0b753a69cc60e20adbd07786150590cafbf34d
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-18 15:11:18 -0800

    add content to intro and styling for down arrow

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,7 +9,14 @@ export default function Intro() {
         </div>
       </div>
       <div className="right">
-
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Cynthia Leung</h1>
+          <h3>Full Stack<span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down-arrow.png" alt="" />
+        </a>
       </div>
     </div>
   )
