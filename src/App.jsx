e65a943e0dffd88dc4fd4fe6c27079808fa139b7
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-17 20:58:51 -0800

    create sass global variable for main color, create state and transition effects for menu in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import "./app.scss";
+import { useState } from "react";
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
 import "./navbar.scss";
+import { LocationCity, Mail } from "@material-ui/icons";
 
-export default function Navbar() {
+export default function Navbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="navbar">
-      
+    // when menuOpen is true, add "active" to className -> "navbar active"
+    <div className={"navbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">hello!</a>
+          <div className="itemContainer">
+            <LocationCity className="icon"/>
+            <span>Vancouver, BC </span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>cynthiaa.leung@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          {/* onclick, set state to opposite of what setMenu currently is */}
+          <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line2"></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
