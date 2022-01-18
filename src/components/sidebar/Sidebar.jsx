fd60ab26a156f2f76db491045b0e12161a1beca7
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-18 13:31:07 -0800

    add comment

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ export default function Sidebar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"sidebar " + (menuOpen && "active")}>
       <ul>
+        {/* on click for any li component, sidebar will be hidden */}
         <li onClick={() => {setMenuOpen(false)}}>
           <a href="#intro">Home</a>
         </li>
