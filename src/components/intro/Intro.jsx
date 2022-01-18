5fa46e648014145cff4884de7585435df031f1ac
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-18 15:44:55 -0800

    install ityped library, add animation to cursor

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Web Developer"]
+    })
+  }, [])
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Cynthia Leung</h1>
-          <h3>Full Stack<span></span></h3>
+          <h3>Full Stack <span ref={textRef}>Web Developer</span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down-arrow.png" alt="" />
