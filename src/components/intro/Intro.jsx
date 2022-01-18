867118c84ef59706374a605737fb27ef2879d7e1
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-18 14:50:06 -0800

    add portrait to intro, add files needed, styling for portrait and img container

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,14 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/portrait.png" alt="portrait"></img>
+        </div>
+      </div>
+      <div className="right">
 
+      </div>
     </div>
   )
 }
