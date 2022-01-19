988e2302ddce8232b00d74810f4d1932ec73bea7
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-18 17:11:47 -0800

    add screenshots needed for portfolio component, implement state to change between tabs, create PortfolioList component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,49 @@
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useEffect, useState } from "react";
 
 export default function Portfolio() {
+
+  const [selected, setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "allProjects",
+      title: "All Projects",
+    }
+  ]
+
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map(item => (
+          <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/featured-EatsyPeasy.png" alt="EatsyPeasy" />
+          <h3>EatsyPeasy</h3>
+        </div>
+        <div className="item">
+          <img src="assets/featured-Gnome-Sweet-Gnome.png" alt="Gnome-Sweet-Gnome" />
+          <h3>Gnome Sweet Gnome</h3>
+        </div>
+        <div className="item">
+          <img src="assets/featured-Scheduler.png" alt="Scheduler" />
+          <h3>Scheduler</h3>
+        </div>
+      </div>
     </div>
   )
 }
